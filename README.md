def8e3f972c135f44af8b6c94e7f9887e192dc8f
Binarion <itmaster@duck.com>
2023-07-05 00:09:08 +0300

    Версия 1.1.5

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
 # gulp-web-images-css
-gulp-web-images-css
+
+> This is a modified version of the plugin [gulp-avif-css](https://www.npmjs.com/package/gulp-avif-css). Generates additional expressions with `.webp` & `.avif` classes and appropriate extension.
+
+## Install
+
+Install `gulp-web-images-css` as a development dependency:
+
+```shell
+npm i -D gulp-web-images-css
+```
+
+## Options
+
+Type: `object`
+
+### extensions
+
+Type: `array`<br>
+Default: `['png', 'jpg', 'jpeg']`
+
+Expansion subject to processing.
+
+### mode
+
+Type: `string`<br>
+Default: `all`<br>
+Possible values:
+   -  «avif» — Add only support «AVIF»
+   -  «webp» — Add only support «Webp»
+   -  «all» — Add support «AVIF» and «Webp»
+
+### unregister
+
+Type: `boolean`<br>
+Default: `true`
+
+Do not distinguish between lowercase and uppercase letters in extensions
+
+## Usage
+
+Add it to your `gulpfile.js`
+
+```javascript
+const gulp = require("gulp");
+const webImagesCSS = require("gulp-web-images-css");
+
+gulp.src("./src/css/*.css").pipe(webImagesCSS()).pipe(gulp.dest("./dist"));
+```
+
+or
+
+```javascript
+const gulp = require("gulp");
+const webImagesCSS = require("gulp-web-images-css");
+
+gulp
+	.src("./src/css/*.css")
+	.pipe(webImagesCSS({mode: "all"}))
+	.pipe(gulp.dest("./dist"));
+```
+
+Include special plugin adds `.avif` and `.webp` classes to body (if it supports) into your JavaScript file (add it into head tag)
+
+```javascript
+import "gulp-web-images-css/plugin";
+```
+
+## Examples
+
+#### Input:
+
+```css
+.box {
+	background-image: url("image.png");
+}
+```
+
+#### Output:
+
+```css
+.box {
+	background-image: url("image.png");
+}
+
+.webp .box {
+	background-image: url("image.webp");
+}
+
+.avif .box {
+	background-image: url("image.avif");
+}
+```
---
+++ index.js
@@ -0,0 +1,118 @@
+const through = require('through2');
+const css = require('css');
+
+class WebImagesCSS {
+  constructor() {
+	 this.unregister = true;
+    this.avaibleExtensions = ['png', 'jpg', 'jpeg'];
+	 this.mode = "all";
+  }
+
+  init = (options = {}) => {
+	Object.assign(this, options, {webExt: ['avif', 'webp']});
+	if (this.unregister) {
+		this.avaibleExtensions = [...new Set(this.extensions.concat(this.extensions.map((it) => it.toUpperCase())))];
+	} else {
+		this.avaibleExtensions = this.extensions;
+	}
+	if (this.mode == "avif") {
+		this.webExt = ['avif'];
+	} else if (this.mode == "webp") {
+		this.webExt = ['webp'];
+	}
+    return through.obj((buffer, _, callback) => {
+      if (buffer.isNull()) {
+        return callback(null, buffer);
+      } else if (buffer.isStream()) {
+        return callback(null, buffer);
+      }
+
+      this.buffer = buffer;
+      this.fileData = buffer.contents.toString();
+      this.transform();
+
+      callback(null, this.buffer);
+    })
+  }
+
+  transform(rules = this.parseCSS()) {
+    rules.forEach((expression) => {
+      if (expression.type === 'media') {
+        return this.transform(expression.rules);
+      } else if (expression.type === 'rule') {
+        expression.declarations
+        .forEach((rule) => {
+          if (!this.isBackground(rule)) return;
+
+          const imageExtension = this.getExtension(rule);
+
+          if (!imageExtension) return;
+
+          this.createExpressions({
+            rule,
+            selectors: expression.selectors,
+            position: expression.type === 'media'
+              ? rule.position
+              : expression.position,
+            imageExtension
+          })
+        })
+      }
+    })
+
+    this.buffer.contents = new Buffer.from(this.fileData);
+  }
+
+  parseCSS() {
+    return css.parse(this.fileData).stylesheet.rules;
+  }
+
+  isBackground({ property, value }) {
+    if (
+      property === 'background' ||
+      property === 'background-image' &&
+      value.indexOf('.webp') < 0 &&
+      value.indexOf('.avif') < 0
+    ) {
+      return true;
+    }
+  }
+
+  getExtension({ value }) {
+    return this.avaibleExtensions.find((extension) => {
+      if (value.indexOf(extension) > 0) {
+        return extension;
+      }
+    })
+  }
+
+  createExpressions({ rule, selectors, position, imageExtension }) {
+    const splitedData = this.fileData.split(/\n/);
+
+    const rowIndex = position.end.line - 1;
+    const columnIndex = position.end.column - 1;
+
+    ;this.webExt.forEach((modernExtension) => {
+      splitedData.splice(rowIndex, 1, this.changeString(
+        splitedData[rowIndex],
+        columnIndex,
+        `
+          .${modernExtension} ${selectors.join('')} {
+            ${rule.property}: ${rule.value.replace(imageExtension, modernExtension)}
+          }
+        `.replaceAll('\n', '')
+        )
+      )
+    })
+
+    this.fileData = splitedData.join('\n');
+  }
+
+  changeString(string, changeIndex, changeText) {
+    return string.substring(0, changeIndex) + changeText + string.substring(changeIndex);
+  }
+}
+
+const webImagesCSS = new WebImagesCSS();
+
+module.exports = webImagesCSS.init;
